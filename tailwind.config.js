aba438743bfa22a0adace3cc9b81689a0f313705
Keith Prinkey <chieftech@worldesports.app>
2023-07-13 20:31:24 -0500

    created git repo for portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './about.html',
+    './disclaimer.html',
+  ],
+
+  theme: {
+    extend: {
+      colors: {
+        primary: '#00ff00',
+        secondary: '#6c757d',
+        success: '#28a745',
+        danger: '#dc3545',
+        warning: '#ffc107',
+        info: '#17a2b8',
+      },
+    },
+  },
+  plugins: [],
+};
+
+
